241215dd0ab5b1adf4434fffb46c946537f0078e
kaze-k <kerkaze@outlook.com>
2024-02-06 11:45:10 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,91 @@
 # fish-proxy
-A fish shell plugin to configure proxy
+
+A [fish shell](https://fishshell.com) plugin to configure proxy
+
+It is the fish version of [zsh-proxy](https://github.com/SukkaW/zsh-proxy), thanks to [Sukka](https://github.com/SukkaW)
+
+## Install
+
+use [fisher](https://github.com/jorgebucaran/fisher)
+
+```fish
+fisher install kaze-k/fish-proxy
+```
+
+----
+
+Congratulations! Open a new terminal. If you see following lines, you have successfully installed `fish-proxy`:
+
+```
+----------------------------------------
+You should run following command first:
+> init_proxy
+----------------------------------------
+```
+
+## Usage
+
+### `init_proxy`
+
+The tip mentioned below will show up next time you open a new terminal if you haven't  initialized the plugin with `init_proxy`.
+
+After you run `init_proxy`, it is time to configure the plugin.
+
+### `config_proxy`
+
+Execute `config_proxy` will lead you to fish-proxy configuration. Fill in socks5 & http proxy address in format `address:port` like `127.0.0.1:1080` & `127.0.0.1:8080`.
+
+Default configuration of socks5 proxy is `127.0.0.1:1080`, and http proxy is `127.0.0.1:8080`. You can leave any of them blank during configuration to use their default configuration.
+
+Currently `fish-proxy` doesn't support proxy with authentication, but I am working on it.
+
+### `proxy`
+
+After you configure the `fish-proxy`, you are good to go. Try following command will enable proxy for supported package manager & software:
+
+```fish
+proxy
+```
+
+And next time you open a new terminal, fish-proxy will automatically enable proxy for you.
+
+### `noproxy`
+
+If you want to disable proxy, you can run following command:
+
+```fish
+noproxy
+```
+
+### `myip`
+
+If you forget whether you have enabled proxy or not, it is fine to run `proxy` command directly, as `proxy` will reset all the proxy before enable them. But the smarter way is to use following command to check which IP you are using now:
+
+```fish
+myip
+```
+
+Check procedure will use `curl` and the IP data come from `ipip.net`, `ip.cn` & `ip.gs`.
+
+## Uninstallation
+
+```fish
+fisher uninstall kaze-k/fish-proxy
+rm -rf ~/.fish-proxy
+```
+
+## Supported
+
+`fish-proxy` currently support those package manager & software:
+
+- `http_proxy`
+- `https_proxy`
+- `ftp_proxy`
+- `rsync_proxy`
+- `all_proxy`
+- git (http)
+- npm & yarn & pnpm
+
+## Thanks
+
+- [@Sukka](https://github.com/SukkaW)
---
+++ conf.d/fish-proxy.fish
@@ -0,0 +1,290 @@
+#!/bin/fish
+#  _____ ___ ____  _   _ ____                      
+# |  ___|_ _/ ___|| | | |  _ \ _ __ _____  ___   _ 
+# | |_   | |\___ \| |_| | |_) | '__/ _ \ \/ / | | |
+# |  _|  | | ___) |  _  |  __/| | | (_) >  <| |_| |
+# |_|   |___|____/|_| |_|_|   |_|  \___/_/\_\\__, |
+#                                            |___/ 
+# -------------------------------------------------
+
+function __read_proxy_config -d "read proxy configuration"
+  set -g __FISHPROXY_STATUS (cat $HOME/.fish-proxy/status)
+  set -g __FISHPROXY_SOCKS5 (cat $HOME/.fish-proxy/socks5)
+  set -g __FISHPROXY_HTTP (cat $HOME/.fish-proxy/http)
+  set -g __FISHPROXY_NO_PROXY (cat $HOME/.fish-proxy/no_proxy)
+  set -g __FISHPROXY_GIT_PROXY_TYPE (cat $HOME/.fish-proxy/git_proxy_type)
+end
+
+function __check_whether_init -d "check whether fish-proxy is initialized"
+  if [ ! -f "$HOME/.fish-proxy/status" ] || [ ! -f "$HOME/.fish-proxy/http" ] || [ ! -f "$HOME/.fish-proxy/socks5" ] || [ ! -f "$HOME/.fish-proxy/no_proxy" ]
+      echo "----------------------------------------"
+      echo "You should run following command first:"
+      echo "> init_proxy"
+      echo "----------------------------------------"
+  else
+      __read_proxy_config
+  end
+end
+
+function __check_ip -d "check your IP"
+  echo "========================================"
+  echo "Check what your IP is"
+  echo "----------------------------------------"
+  set -l ipv4 $(curl -s -k https://api-ipv4.ip.sb/ip -H 'user-agent: fish-proxy')
+  if [ "$ipv4" != "" ]
+      echo "IPv4: $ipv4"
+  else
+      echo "IPv4: -"
+  end
+  echo "----------------------------------------"
+  set -l ipv6 $(curl -s -k -m10 https://api-ipv6.ip.sb/ip -H 'user-agent: fish-proxy')
+  if [ "$ipv6" != "" ]
+      echo "IPv6: $ipv6"
+  else
+      echo "IPv6: -"
+  end
+  if command -v python >/dev/null
+      set -l geoip $(curl -s -k https://api.ip.sb/geoip -H 'user-agent: fish-proxy')
+      if [ "$geoip" != "" ]
+          echo "----------------------------------------"
+          echo "Info: "
+          echo "$geoip" | python -m json.tool
+      end
+  end
+  echo "========================================"
+end
+
+function __config_proxy -d "config proxy"
+  echo "========================================"
+  echo "FISH Proxy Plugin Config"
+  echo "----------------------------------------"
+
+  echo -n "[socks5 proxy] {Default as 127.0.0.1:1080}
+(address:port): "
+  read -l __read_socks5
+
+  echo -n "[socks5 type] Select the proxy type you want to use {Default as socks5}:
+1. socks5
+2. socks5h (resolve DNS through the proxy server)
+(1 or 2): "
+  read -l __read_socks5_type
+
+  echo -n "[http proxy]   {Default as 127.0.0.1:8080}
+(address:port): "
+  read -l __read_http
+
+  echo -n "[no proxy domain] {Default as 'localhost,127.0.0.1,localaddress,.localdomain.com'}
+(comma separate domains): "
+  read -l __read_no_proxy
+
+  echo -n "[git proxy type] {Default as socks5}
+(socks5 or http): "
+  read -l __read_git_proxy_type
+  echo "========================================"
+
+  if [ -z "$__read_socks5" ]
+      set __read_socks5 "127.0.0.1:1080"
+  end
+  if [ -z "$__read_socks5_type" ]
+      set __read_socks5_type "1"
+  end
+  if [ -z "$__read_http" ]
+      set __read_http "127.0.0.1:8080"
+  end
+  if [ -z "$__read_no_proxy" ]
+      set __read_no_proxy "localhost,127.0.0.1,localaddress,.localdomain.com"
+  end
+  if [ -z "$__read_git_proxy_type" ]
+      set __read_git_proxy_type "socks5"
+  end
+
+  echo "http://$__read_http" >"$HOME/.fish-proxy/http"
+  if [ "$__read_socks5_type" = "2" ]
+      echo "socks5h://$__read_socks5" >"$HOME/.fish-proxy/socks5"
+  else
+      echo "socks5://$__read_socks5" >"$HOME/.fish-proxy/socks5"
+  end
+  echo "$__read_no_proxy" >"$HOME/.fish-proxy/no_proxy"
+  echo "$__read_git_proxy_type" >"$HOME/.fish-proxy/git_proxy_type"
+
+  __read_proxy_config
+end
+
+# Proxy for terminal
+
+function __enable_proxy_all -d "enable proxy for all"
+  # http_proxy
+  set -gx http_proxy "$__FISHPROXY_HTTP"
+  set -gx HTTP_PROXY "$__FISHPROXY_HTTP"
+  # https_proxy
+  set -gx https_proxy "$__FISHPROXY_HTTP"
+  set -gx HTTPS_PROXY "$__FISHPROXY_HTTP"
+  # ftp_proxy
+  set -gx ftp_proxy "$__FISHPROXY_HTTP"
+  set -gx FTP_PROXY "$__FISHPROXY_HTTP"
+  # rsync_proxy
+  set -gx rsync_proxy "$__FISHPROXY_HTTP"
+  set -gx RSYNC_PROXY "$__FISHPROXY_HTTP"
+  # all_proxy
+  set -gx ALL_PROXY "$__FISHPROXY_SOCKS5"
+  set -gx all_proxy "$__FISHPROXY_SOCKS5"
+
+  set -gx no_proxy "$__FISHPROXY_NO_PROXY"
+end
+
+function __disable_proxy_all -d "disable proxy for all"
+  set -e http_proxy
+  set -e HTTP_PROXY
+  set -e https_proxy
+  set -e HTTPS_PROXY
+  set -e ftp_proxy
+  set -e FTP_PROXY
+  set -e rsync_proxy
+  set -e RSYNC_PROXY
+  set -e ALL_PROXY
+  set -e all_proxy
+  set -e no_proxy
+end
+
+# Proxy for Git
+
+function __enable_proxy_git -d "enable proxy for git"
+  if [ "$__FISHPROXY_GIT_PROXY_TYPE" = "http" ]
+      git config --global http.proxy "$__FISHPROXY_HTTP"
+      git config --global https.proxy "$__FISHPROXY_HTTP"
+  else
+      git config --global http.proxy "$__FISHPROXY_SOCKS5"
+      git config --global https.proxy "$__FISHPROXY_SOCKS5"
+  end
+end
+
+function __disable_proxy_git -d "disable proxy for git"
+  git config --global --unset http.proxy
+  git config --global --unset https.proxy
+end
+
+# NPM
+
+function __enable_proxy_npm -d "enable proxy for npm"
+  if command -v npm >/dev/null
+      npm config set proxy "$__FISHPROXY_HTTP"
+      npm config set https-proxy "$__FISHPROXY_HTTP"
+      echo "- npm"
+  end
+  if command -v yarn >/dev/null
+      yarn config set proxy "$__FISHPROXY_HTTP" >/dev/null 2>&1
+      yarn config set https-proxy "$__FISHPROXY_HTTP" >/dev/null 2>&1
+      echo "- yarn"
+  end
+  if command -v pnpm >/dev/null
+      pnpm config set proxy "$__FISHPROXY_HTTP" >/dev/null 2>&1
+      pnpm config set https-proxy "$__FISHPROXY_HTTP" >/dev/null 2>&1
+      echo "- pnpm"
+  end
+end
+
+function __disable_proxy_npm -d "disable proxy for npm"
+  if command -v npm >/dev/null
+      npm config delete proxy
+      npm config delete https-proxy
+  end
+  if command -v yarn >/dev/null
+      yarn config delete proxy >/dev/null 2>&1
+      yarn config delete https-proxy >/dev/null 2>&1
+  end
+  if command -v pnpm >/dev/null
+      pnpm config delete proxy >/dev/null 2>&1
+      pnpm config delete https-proxy >/dev/null 2>&1
+  end
+end
+
+# ==================================================
+
+function __enable_proxy -d "enable proxy"
+  if [ -z "$__FISHPROXY_STATUS" ] || [ -z "$__FISHPROXY_SOCKS5" ] || [ -z "$__FISHPROXY_HTTP" ]
+      echo "========================================"
+      echo "fish-proxy can not read configuration."
+      echo "You may have to reinitialize and reconfigure the plugin."
+      echo "Use following commands would help:"
+      echo "> init_proxy"
+      echo "> config_proxy"
+      echo "> proxy"
+      echo "========================================"
+  else
+      echo "========================================"
+      echo -n "Resetting proxy... "
+      __disable_proxy_all
+      __disable_proxy_git
+      __disable_proxy_npm
+      echo "Done!"
+      echo "----------------------------------------"
+      echo "Enable proxy for:"
+      echo "- shell"
+      __enable_proxy_all
+      echo "- git"
+      __enable_proxy_git
+      # npm & yarn & pnpm"
+      __enable_proxy_npm
+      echo "Done!"
+  end
+end
+
+function __disable_proxy -d "disable proxy"
+  __disable_proxy_all
+  __disable_proxy_git
+  __disable_proxy_npm
+end
+
+function __auto_proxy -d "auto proxy"
+  if [ "$__FISHPROXY_STATUS" = "1" ]
+      __enable_proxy_all
+  end
+end
+
+# ==================================================
+
+function init_proxy -d "initialize fish-proxy"
+  mkdir -p "$HOME/.fish-proxy"
+  touch "$HOME/.fish-proxy/status"
+  echo "0" >"$HOME/.fish-proxy/status"
+  touch "$HOME/.fish-proxy/http"
+  touch "$HOME/.fish-proxy/socks5"
+  touch "$HOME/.fish-proxy/no_proxy"
+  touch "$HOME/.fish-proxy/git_proxy_type"
+  echo "----------------------------------------"
+  echo "Great! The fish-proxy is initialized"
+  echo ""
+  echo -E '  _____ ___ ____  _   _ ____                       '
+  echo -E ' |  ___|_ _/ ___|| | | |  _ \ _ __ _____  ___   _  '
+  echo -E " | |_   | |\___ \| |_| | |_) | '__/ _ \ \/ / | | | "
+  echo -E ' |  _|  | | ___) |  _  |  __/| | | (_) >  <| |_| | '
+  echo -E ' |_|   |___|____/|_| |_|_|   |_|  \___/_/\_\\__, | '
+  echo -E '                                            |___/  '
+  echo "----------------------------------------"
+  echo "Now you might want to run following command:"
+  echo "> config_proxy"
+  echo "----------------------------------------"
+end
+
+function config_proxy -d "config proxy"
+  __config_proxy
+end
+
+function proxy -d "enable proxy"
+  echo "1" >"$HOME/.fish-proxy/status"
+  __enable_proxy
+  __check_ip
+end
+
+function noproxy -d "disable proxy"
+  echo "0" >"$HOME/.fish-proxy/status"
+  __disable_proxy
+  __check_ip
+end
+
+function myip -d "show your IP"
+  __check_ip
+end
+
+__check_whether_init
+__auto_proxy
